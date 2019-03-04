79ac2bb83ee05058879a63801bfdd6a006b2629b
jericho <boker_boker@yahoo.com>
2019-03-04 21:11:56 +1100

    create react-redux-saga tutorial sample

---
+++ src/sagas.js
@@ -0,0 +1,26 @@
+import { takeLatest, call, put } from 'redux-saga/effects';
+import axios from 'axios';
+
+export function* watcherSaga() {
+  yield takeLatest('API_CALL_REQUEST', workerSaga);
+}
+
+function fetchDog() {
+  return axios({
+    method: 'get',
+    url: 'https://dog.ceo/api/breeds/image/random'
+  });
+}
+
+function* workerSaga() {
+  try {
+    const response = yield call(fetchDog);
+    const dog = response.data.message;
+
+    //dispatch a success action to the store with the new dog
+    yield put({ type: 'API_CALL_SUCCESS', dog });
+  } catch (error) {
+    // dispatch a failure action to the store with the error
+    yield put({ type: 'API_CALL_FAILURE', error });
+  }
+}
